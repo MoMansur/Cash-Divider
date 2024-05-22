32f448a42513e2868ad85f879c0b0158e8fe9261
Mansur Shehu <mansurshehumsm@gmail.com>
2024-05-22 13:45:40 +0300

    Simple

--- dom.js
+++ dom.js
@@ -1,8 +1,6 @@
 
 import { calc, deleeFunc} from "./main.js";
-import { getCalcArr } from "./main.js";
-
-
+import { DivisionClickCalculation } from "./main.js";
 
 
 const modelSpace= document.getElementById('modelSpace')
@@ -10,14 +8,16 @@ const getCalcDiv = document.getElementById('CalcDivs')
 
 
 
-     function factoryDom(card, type, id, className, ){
+    export function factoryDom(type, id, className,value){
         const element = document.createElement(type)
-        element.innerText = name;
         element.id = id
         element.className = className
-     
+        element.value = value
 
-        card.append(element)
+        function appender(card){
+            card.append(element)
+        }
+        
 
         function setInnerText(innerText){
             element.innerText = innerText
@@ -32,112 +32,129 @@ const getCalcDiv = document.getElementById('CalcDivs')
                 eventFunction();
             });
         }
-        function setWidth(width){
-            element.style.setWidth = width;
+        function setWidth(width,height){
+            element.style.width = width;
+            element.style.height = height;
+        }
+        function otherCss(border,Rborder, padding,tAlign){
+            element.style.border = border
+            element.style.borderRadius = Rborder
+            element.style.padding = padding
+            element.style.textAlign = tAlign
         }
     
-        return{element,setBgColor, setInnerText, eventListener,setWidth}
+        return{element,appender,setBgColor, setInnerText, eventListener,setWidth, otherCss}
     }
 
- export const modelCreator = ((name, type, amount, divInto, index)=>{
+ export const modelCreator = ((name, type, amount,  divInto, index, currencySelect)=>{
 
 
     const card = document.createElement('div')
     card.id = 'card';
     card.setAttribute(`data-index`, index)
-
     modelSpace.append(card)
 
- 
-        const firstLineElem = factoryDom(card, 'div', 'firstLine', 'firstLine',)
-    
-       
+        const firstLineElem = factoryDom('div', 'firstLine', 'firstLine',)   
         const firstLine = firstLineElem.element
 
         //DATA
-        const nameSpan = factoryDom(firstLine, 'span', 'nameSpan', 'formDataSpan')
+        const nameSpan = factoryDom('span', 'nameSpan', 'formDataName')
         nameSpan.setInnerText(name)
-        const typeSpan = factoryDom(firstLine,  'span','typeSpan', 'formDataSpan',)
+        nameSpan.setWidth('550px')
+        nameSpan.otherCss('1px solid white')
+        nameSpan.setBgColor('black')
+
+
+        const typeSpan = factoryDom('span','typeSpan', 'formDataSpan',)
         typeSpan.setInnerText(type)
+        if(type === 'Debt'){
+            typeSpan.setBgColor('rgb(161 98 7)')
+        }else{
+            typeSpan.setBgColor('rgb(22 78 99)')
+        }
 
-        const amountSpan = factoryDom(firstLine,  'span','amountSpan', 'formDataSpan')
-        amountSpan.setInnerText(amount)  
-        const divIntoSpan = factoryDom(firstLine,  'span','divIntoSpan', 'formDataSpan')
+        const amountSpan = factoryDom('span','amountSpan', 'formDataSpan')
+        amountSpan.setInnerText(currencySelect+amount)  
+
+        const divIntoSpan = factoryDom('span','divIntoSpan', 'formDataSpan')
         divIntoSpan.setInnerText(divInto)
-        //BUTTONS
-        const viewBtn = factoryDom(firstLine, 'viewbtn', 'DeleteBtn', 'btn btn-success')
-        viewBtn.setInnerText('Status')
-         //Delete Button
-        const deleteButton =factoryDom(firstLine, 'button', 'DeleteBtn', 'btn btn-danger',)
-        deleteButton.setInnerText('Delete Card')
-       
-        deleteButton.eventListener(() => {
-            deleeFunc(card); 
-        });
-      
-        
-   
-
-
-  //SECOND LINE
-        const secondLineElement = factoryDom(card, 'div', 'secondLine', 'secondLine',)
-      
-       
+        divIntoSpan.setWidth('80px')
+     
+    
+        //SECOND LINE
+        const secondLineElement = factoryDom('div', 'secondLine', 'secondLine',)
         const secondLine = secondLineElement.element
 
-        const calcInformations = factoryDom(secondLine,  'div','CalcInfo', 'CalcInfo',)
-        calcInformations.setInnerText('Information here')
+        const cardStats = factoryDom( 'div', 'cardStats', 'cardStats')
+        const getCardStats = cardStats.element
+           
+            const statusSpan = factoryDom( 'p', 'statusSpan', 'statusSpan')
+            statusSpan.setBgColor('black')
+            statusSpan.setWidth('300px','50px')
+            statusSpan.otherCss('1px solid white', '10px', '5px','center' )
+    
+            //Delete Button
+           const deleteButton =factoryDom('button', 'DeleteBtn', 'btn btn-danger',)
+           deleteButton.setInnerText('Delete Card')
+        
+            deleteButton.eventListener(() => {
+                deleeFunc(card); 
+            });
+
+
+     
+           
+        const calcDiv = factoryDom('div', 'CalcDivs', 'CalcDivs')
+        const getCalcDiv = calcDiv.element 
+
+        let total = 0;
+          const divisionProcessCard = (()=>{
+            statusSpan.setInnerText(`${currencySelect}${total} /${currencySelect}${amount}`)
+       
+          
+            getCalcDiv.addEventListener('click', (event) => {
+
+                DivisionClickCalculation(event, statusSpan, currencySelect, total, amount)
+                
+                    })
+
+                    calc(amount, divInto, getCalcDiv, currencySelect)
+
+   
+
+          })()
+          
+          const DOMbgColors = () =>{}
+        
+          DOMbgColors()
+
+          const DOMappenders = () =>{
+            firstLineElem.appender(card)
+            nameSpan.appender(firstLine)
+            typeSpan.appender(firstLine)
+            amountSpan.appender(firstLine)
+            divIntoSpan.appender(firstLine)
+
+
+            secondLineElement.appender(card)
+            calcDiv.appender(secondLine)
+
+            cardStats.appender(secondLine)
+
+            statusSpan.appender(getCardStats)
+            deleteButton.appender(getCardStats)
+
+
+
+          }
+
+    
+
+
+          DOMappenders()
      
 
-        const spanData = document.createElement('span')
-        spanData.className - 'spanData'
-        spanData.innerText = amount
-        spanData.style.backgroundColor = 'Green'
-        
-        
-           
-          
-
-            const calcDiv = factoryDom(secondLine, 'div', 'CalcDivs', 'CalcDivs')
-            const getCalcDiv = calcDiv.element 
-
-            const cardtStats = factoryDom(secondLine, 'div', 'cardtStats', 'cardtStats')
-            
-
-
-            getCalcDiv.addEventListener('click', (e)=>{
-                const theNode = e.target;
-                const theNumber = parseInt(theNode.textContent)
-
-                console.dir(theNode)
-
-                let isOn = true
-
-                if(isOn){
-                    getCalcDiv.style.backgroundColor = 'green'
-                }else{
-                    getCalcDiv.style.backgroundColor = 'red'
-                }
-                 isOn = !isOn
-
-
-            })
-
-            function statsCollection(){
-
-            }
-            
-            console.log(amount)
-            
-
-            
-       
-
-calc(amount, divInto, getCalcDiv)
-
-        
-
-
+return {divisionProcessCard}
 
 });
 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import modelCreator from "./dom.js";
-
+import { factoryDom } from "./dom.js";
 
 const dataArray = []
 class Data{
@@ -11,15 +11,12 @@ class Data{
     }  
 }
 
-
 function addCard(name, type, amount, divInto){
     const newData = new Data(name, type, amount, divInto)
 
     modelCreator(name,type,amount,divInto)
     dataArray.push(newData)
 }
-
-
 
 dataArray.push({
     name: 'Bills', 
@@ -30,36 +27,85 @@ dataArray.push({
 
 const calcArr = [];
 
-
-export const  calc = (amount, divideInto, theDiv) =>{
-    let sum = amount / divideInto;
-    calcArr.length = 0;
-
-    for (let i = 0; i < divideInto; i++) {
-        calcArr.push(sum);
-        
-       
-    } 
-    theDiv.innerHTML = '';
-
-    // Append new results
-    for (let j = 0; j < calcArr.length; j++) {
-        theDiv.innerHTML += `<span>${calcArr[j]}</span>`;
+function currencySign(currencySelect){
+    let amountSign=''
+    switch (currencySelect) {
+        case 'usd':
+            amountSign = '$';
+            break;
+        case 'eur':
+            amountSign = '€';
+            break;
+        case 'gbp':
+            amountSign = '£';
+            break;
+        case 'jpy':
+            amountSign = '¥';
+            break;
+        case 'try':
+            amountSign = '₺';
+            break;
+        default:
+            amountSign = '';
     }
 
+    return amountSign
+}
 
+export const  calc = (amount, divideInto, theDiv,currencySelect) =>{
+     let sum = Math.round(amount / divideInto)
+        calcArr.length = 0;
+    for (let i = 0; i < divideInto; i++) {
+        calcArr.push(sum);
+    } 
+    theDiv.innerHTML = '';
+    // Append new results
+    for (let j = 0; j < calcArr.length; j++) {
+        const eachDivision = factoryDom('span', 'division', 'division', calcArr[j])
+        eachDivision.setInnerText(currencySelect + calcArr[j])
+        eachDivision.appender(theDiv)
+    }
+   
 
 }
 
 
-// Example usage
+export function DivisionClickCalculation(event, theDiv, currencySelect, total, amount){
+    if (event.target.classList.contains('division')) {
+        const clickedSpan = event.target;  
+        const clickedValue = clickedSpan.value
 
+        console.log(clickedValue)
+        let clickCount = clickedSpan.dataset.clickCount || 0;  
+        clickCount++;  
+        clickedSpan.dataset.clickCount = clickCount;
+   
+            if(clickCount % 2 === 0 ){
+            total -= clickedValue;
+            clickedSpan.style.backgroundColor = 'rgb(44, 42, 39)'
+            theDiv.setInnerText(`${currencySelect}${total} / ${currencySelect}${amount}`)
+            console.log(clickedValue)
+        }else{
+            total += clickedValue;
+            clickedSpan.style.backgroundColor = 'rgb(22 101 52)'
+            theDiv.setInnerText(`${currencySelect}${total} / ${currencySelect}${amount}`)
+        }
+        }
 
+        if(total == amount){
+            theDiv.setInnerText(`Completed`)
+            theDiv.setBgColor('green')
+        }else{
+            theDiv.setBgColor('rgb(136, 12, 12)')
+        }
+}
 
+let getCurrency = '$'
 
-function displayer(){  
+function displayer(currencySelect){  
+
     for(let i = 0; i<dataArray.length; i++){
-        modelCreator(dataArray[i].name, dataArray[i].type, dataArray[i].amount, dataArray[i].divInto, i)
+        modelCreator(dataArray[i].name, dataArray[i].type, dataArray[i].amount, dataArray[i].divInto, i,currencySelect)
         
     }
 
@@ -69,7 +115,7 @@ export const getCalcArr = (() => {return calcArr})();
 
 export function deleeFunc(model){
     const index = parseInt(model.getAttribute("data-index"));
-           var confirmToRemove = confirm('Are you sure you want to remove this book')
+           var confirmToRemove = confirm('Are you sure you want to remove this Card')
        
            if(confirmToRemove){
                dataArray.splice(index, 1);
@@ -82,23 +128,25 @@ export function deleeFunc(model){
 
 }
 
-displayer()
-
+displayer(getCurrency)
 
 function formCaller(){
-
+    let getCurrency = currencySign(currencySelect.value)
+    console.log(getCurrency)
     addCard(nameInput.value, typeSelect.value, amountInput.value, divIntoInput.value)
 
     modelSpace.innerHTML = "";
-    displayer()
-    calc(amountInput.value, divIntoInput.value);
+    displayer(getCurrency)
+    calc(amountInput.value, divIntoInput.value );
+    
 }
-
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
+    
+  
+   
     formCaller()
+    form.reset();
 
-
-    form.reset()
    })
